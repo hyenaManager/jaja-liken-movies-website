85b7a4a33ba850843bc0e299ae3504aa8f757a8a
hyenaManager <zodiacjelly9@gmail.com>
2023-08-27 19:59:12 +0630

    modify: change image UI search component

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -59,23 +59,9 @@ export default function Search() {
 
 function MoviePoster({ movie }) {
   const [isHover, setIsHover] = useState(false);
-  const [imgSrc, setImgSrc] = useState(null);
-  const [seeDetail, setSeeDetail] = useState(false);
-
-  useEffect(() => {
-    // Load the image source when the component mounts
-    const source = "https://image.tmdb.org/t/p/original" + movie.poster_path;
-    const img = new Image();
-    img.src = source;
-    img.onload = () => {
-      setImgSrc(source);
-    };
-  }, [movie.poster_path]);
-
-  if (!imgSrc) {
-    return <div className={` bg-opacity-50 rounded-md bg-red-900 `}></div>;
-  }
-
+  const [imageIsLoaded, setImageIsLoaded] = useState(false);
+  const [imageNotFound, setImageNotFound] = useState(false);
+  const link = "https://image.tmdb.org/t/p/original";
   return (
     <motion.div
       initial={{ opacity: 0.5 }}
@@ -85,21 +71,37 @@ function MoviePoster({ movie }) {
       onMouseLeave={() => setIsHover(false)}
     >
       {/* Display the movie details */}
-
       <motion.img
         loading="lazy"
         initial={{ opacity: 0.5 }}
-        animate={{ opacity: 1 }}
-        src={imgSrc}
-        className="rounded-md"
+        animate={{ opacity: imageIsLoaded ? 1 : 0 }}
+        src={link + movie.poster_path}
+        onLoad={() => setImageIsLoaded(true)}
+        className="rounded-md z-10"
+        onError={() => setImageNotFound(true)}
       />
+      {/* place holder image */}
+
+      <div
+        className={` bg-opacity-50 rounded-md bg-red-900 absolute w-full h-full`}
+      ></div>
+      {imageNotFound && (
+        <div
+          className={`w-full h-full bg-opacity-50 rounded-md bg-red-900 absolute flex flex-col justify-center items-center`}
+        >
+          <span className=" font-kanit text-lg text-white ">{movie.title}</span>
+          <span className=" font-kanit text-lg text-white ">
+            Image Not Available ❕
+          </span>
+        </div>
+      )}
 
       {/* Check movie button */}
       {isHover && (
         <motion.button
           whileHover={{ scale: 1.2 }}
           whileTap={{ scale: 1.3 }}
-          className="p-1 pr-2 pl-2 bg-green-400 text-white rounded-3xl text-md absolute top-3 right-3 drop-shadow-xl"
+          className=" z-20 p-1 pr-2 pl-2 bg-green-400 text-white rounded-3xl text-md absolute top-3 right-3 drop-shadow-xl"
         >
           <Link to={`/search/${movie.id}`} className="text-white">
             Check
