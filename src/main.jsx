401f1df5a811e64cbe6758ed4eb4e3d23943444d
hyenaManager <zodiacjelly9@gmail.com>
2023-08-29 13:09:19 +0630

    add: anylytic vercel tool

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,10 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { inject } from "@vercel/analytics/*";
 
 const queryClient = new QueryClient();
+inject();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
